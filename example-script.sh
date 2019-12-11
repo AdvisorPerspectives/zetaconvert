6e1c0bb9053af602162028eef825af5faa647c4d
Steve Pfisterer <steve@stoutlogic.com>
2019-12-11 10:07:57 -0600

    Rename example shell script

--- example-script.sh
+++ example-script.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 node apconvert events_20191126.jsonl --clicked --url="https://www.allianzlife.com/for-financial-professionals/ria?cmpid=7013g000000LYakAAG-pp-ria-ria-19--ln-191015" --campaign="DEDI: Allianz resend 201911" --output="events_20191126-allianzlife.csv"
 
 node apconvert events_20191126.jsonl --clicked --url="white_papers" --campaign="DEDI: Allianz resend 201911" --output="events_20191126-whitepaper.csv"
