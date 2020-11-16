3be351b54d290eb94297b535c66a50bc5438dc85
Steve Pfisterer <steve@stoutlogic.com>
2020-11-16 15:38:40 -0600

    support both --opened and --clicked in the same command

--- README.md
+++ README.md
@@ -1,62 +1,94 @@
 # zetaconvert
+
 Convert and Filter Zeta event JSON files to CSV
 
 ## Install
+
 ```
 npm install
 ```
 
 ## Execution
+
 ```
 node zetaconvert <file> [options]
 ```
+
 Will generate a csv file in the same directory with `.csv` appended to the end.
 default.
 
 ### Create a shell script
+
 See `example-script.sh` for examples of how to create a shell script to run multiple converts at a time, and to easily construct them in a file. Create your own `.sh` file and then run with with `bash` like:
 
 ```bash
 bash example-script.sh
 ```
+
 but substitute your own file.
 
 ## Output
+
 A CSV file will be outputted with filtered results with the following columns:
 
-| Date | Campaign Name | Event | Url | Email |
-| ---- | ---- | ---- | ---- | ---- |
-|2019-11-26|DEDI: resend 201911|campaign_clicked|https://www.google.com|stevepfisterer@gmail.com|
+| Date       | Campaign Name       | Event            | Url                    | Email                    |
+| ---------- | ------------------- | ---------------- | ---------------------- | ------------------------ |
+| 2019-11-26 | DEDI: resend 201911 | campaign_clicked | https://www.google.com | stevepfisterer@gmail.com |
 
 ## Options
 
 ### Output FIle
+
 Specify an output filename. If not specified, the input file name will be used with `.csv` added to the end of it.
+
 ```
 --output="events_20191126-whitepaper.csv"
 ```
 
-
 #### Append
+
 Append output to a file rather than overwriting it
+
 ```
 --append
 ```
 
 #### URL
+
 Action contains a certain URL
+
 ```
 --url="http://www.google.com`
 ```
 
 #### Campaign
+
 Action from a certain campaign
+
 ```
---campaign="DEDI: Allianz resend 201911" 
+--campaign="DEDI: Allianz resend 201911"
+```
+
+#### Opened
+
+Action is a open
+
+```
+--opened
 ```
 
 #### Clicked
+
 Action is a click
+
 ```
 --clicked
+```
+
+### Combined Opened and Clicked
+
+To save time, if you want to combine opens and clicks in the same file, appened both to the same command
+
+```
+--opened --clicked
 ```
--- zetaconvert.js
+++ zetaconvert.js
@@ -1,51 +1,50 @@
 #!/usr/bin/env node
-const yargs = require('yargs');
-const fs = require('fs');
-const lineReader = require('line-reader');
+const yargs = require("yargs");
+const fs = require("fs");
+const lineReader = require("line-reader");
 
 // Define command line arguments
 const argv = yargs
-    .scriptName("zetaconvert")
-    .usage('$0 <file> [args]', false, (yargs) => {
-      yargs.positional('file', {
-        type: 'string',
-        describe: 'input json file'
-      })
-    })
-    .option('url', {
-      alias: 'u',
-      type: 'string',
-      description: 'Url to search for'
-    })
-    .option('campaign', {
-      alias: 'c',
-      type: 'string',
-      description: 'Campaign to search for'
-    })
-    .option('clicked', {
-      type: 'boolean',
-      description: 'Clicked events'
-    })
-    .option('opened', {
-      type: 'boolean',
-      description: 'Opened events'
-    })
-    .option('output', {
-      alias: 'o',
-      type: 'string',
-      description: 'Output file name'
-    })
-    .option('append', {
-      alias: 'a',
-      type: 'boolean',
-      description: 'Append to output file instead of overwrite.'
-    })
-    .help()
-    .argv;
+  .scriptName("zetaconvert")
+  .usage("$0 <file> [args]", false, (yargs) => {
+    yargs.positional("file", {
+      type: "string",
+      describe: "input json file",
+    });
+  })
+  .option("url", {
+    alias: "u",
+    type: "string",
+    description: "Url to search for",
+  })
+  .option("campaign", {
+    alias: "c",
+    type: "string",
+    description: "Campaign to search for",
+  })
+  .option("clicked", {
+    type: "boolean",
+    description: "Clicked events",
+  })
+  .option("opened", {
+    type: "boolean",
+    description: "Opened events",
+  })
+  .option("output", {
+    alias: "o",
+    type: "string",
+    description: "Output file name",
+  })
+  .option("append", {
+    alias: "a",
+    type: "boolean",
+    description: "Append to output file instead of overwrite.",
+  })
+  .help().argv;
 
 // Set input / output file names
 const inputFile = argv.file;
-const outputFile = argv.output ? argv.output : argv.file + '.csv';
+const outputFile = argv.output ? argv.output : argv.file + ".csv";
 
 // Run program
 try {
@@ -54,16 +53,25 @@ try {
   }
   parseInputFile();
 } catch (err) {
-  console.error(err)
+  console.error(err);
 }
 
 // Only process records where this function returns true
 function filterLine(line) {
-  if (argv.opened && line.indexOf('campaign_opened') < 0) {
+  if (
+    argv.opened &&
+    argv.clicked &&
+    line.indexOf("campaign_opened") < 0 &&
+    line.indexOf("campaign_clicked") < 0
+  ) {
     return false;
   }
-  
-  if (argv.clicked && line.indexOf('campaign_clicked') < 0) {
+
+  if (argv.opened && line.indexOf("campaign_opened") < 0 && !argv.clicked) {
+    return false;
+  }
+
+  if (argv.clicked && line.indexOf("campaign_clicked") < 0 && !argv.opened) {
     return false;
   }
 
@@ -71,7 +79,7 @@ function filterLine(line) {
     return false;
   }
 
-  if (argv.url && line.indexOf(argv.url.replace(/\//g, '\\/')) < 0) {
+  if (argv.url && line.indexOf(argv.url.replace(/\//g, "\\/")) < 0) {
     return false;
   }
 
@@ -83,53 +91,79 @@ function parseInputFile() {
   let convertedLines = 0;
   let totalLines = 0;
 
-  let writeStream = fs.createWriteStream(outputFile, {flags:'a'},  function(err) {
+  let writeStream = fs.createWriteStream(outputFile, { flags: "a" }, function (
+    err
+  ) {
     if (err) {
-      return console.error('write', err);
+      return console.error("write", err);
     }
   });
 
-  let readStream = fs.createReadStream(inputFile,  function(err) {
+  let readStream = fs.createReadStream(inputFile, function (err) {
     if (err) {
-      return console.error('read', err);
+      return console.error("read", err);
     }
   });
 
   const outputFileExist = fs.existsSync(outputFile);
 
   // For Each Input File Line
-  lineReader.eachLine(readStream, function (line) {
-      if (filterLine(line)) { // Process only certain lines
+  lineReader.eachLine(
+    readStream,
+    function (line) {
+      if (filterLine(line)) {
+        // Process only certain lines
         convertedLines++;
 
         // Write CSV Headers
-        if ((!argv.append  && convertedLines === 1) || (argv.append && !outputFileExist)) {
-          writeStream.write('"Date","Campaign Name","Event","Url","Email"' + '\r\n');
+        if (
+          (!argv.append && convertedLines === 1) ||
+          (argv.append && !outputFileExist)
+        ) {
+          writeStream.write(
+            '"Date","Campaign Name","Event","Url","Email"' + "\r\n"
+          );
         }
 
         // Convert line to JSON
         const json = JSON.parse(line);
 
         // Write CSV Row
-        const date =  json.event_time.split('T')[0];
+        const date = json.event_time.split("T")[0];
 
-        writeStream.write('"' + date + '","' + json.campaign_name + '","' + json.event_type + '","' + json.url + '","' + json.email + '"\r\n');
+        writeStream.write(
+          '"' +
+            date +
+            '","' +
+            json.campaign_name +
+            '","' +
+            json.event_type +
+            '","' +
+            json.url +
+            '","' +
+            json.email +
+            '"\r\n'
+        );
       }
 
-    totalLines++;
-  }, function () {
-
-    // DONE: Output end, stats
-    console.log(outputFile + ((!outputFileExist || !argv.append) ? ' created' : ' appended to'));
-    console.log('Converted: ' + convertedLines + ' of ' + totalLines + ' rows');
-  });
+      totalLines++;
+    },
+    function () {
+      // DONE: Output end, stats
+      console.log(
+        outputFile +
+          (!outputFileExist || !argv.append ? " created" : " appended to")
+      );
+      console.log(
+        "Converted: " + convertedLines + " of " + totalLines + " rows"
+      );
+    }
+  );
 }
 
-function clearOutputFile()
-{
+function clearOutputFile() {
   // Clear CSV file
-  let fd = fs.openSync(outputFile, 'w');
-  fs.writeFileSync(fd, '');
+  let fd = fs.openSync(outputFile, "w");
+  fs.writeFileSync(fd, "");
   fs.closeSync(fd);
 }
-
